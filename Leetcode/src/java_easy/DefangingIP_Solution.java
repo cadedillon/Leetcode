97781db3bd4be9fde1234295c20897b977a16599
Cade.dillon <Cade.dillon@KSS-LT-037.kirklandoffice.com>
2020-08-19 09:58:01 -0700

    Initial Commit, practicing basic Java solutions and Egit structure. Will be updating continuously with both Java and Python solutions.

---
+++ Leetcode/src/java_easy/DefangingIP_Solution.java
@@ -0,0 +1,12 @@
+package java_easy;
+
+public class DefangingIP_Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
---
+++ Leetcode/src/java_easy/NumGoodPairs_Solution.java
@@ -0,0 +1,25 @@
+package java_easy;
+
+public class NumGoodPairs_Solution {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,1,1,3};
+		int method_call = numIdenticalPairs(nums);
+		System.out.print(method_call);
+
+	}
+	
+	public static int numIdenticalPairs(int[] nums) {
+        int output = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    output++;
+                }
+            }
+        }
+        
+        return output;
+    }
+
+}
