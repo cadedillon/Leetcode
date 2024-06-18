d8ba38f22ae6bb8dcacc27275fd9ad8b6ad75b6b
Cade Dillon <cade.dillon@outlook.com>
2024-06-17 17:01:56 -0700

    Completed Two Pointer easy ValidAnagram

--- TwoPointer/ValidPalindrome.js
+++ TwoPointer/ValidPalindrome.js
@@ -3,7 +3,47 @@ class Solution {
    * @param {string} s
    * @return {boolean}
    */
-  isPalindrome(s) {}
+
+  isPalindrome(s) {
+    let i = 0;
+    let j = s.length - 1;
+
+    while (j > i) {
+      if (!isAlphaNum(s.charAt(i))) {
+        i++;
+        continue;
+      }
+
+      if (!isAlphaNum(s.charAt(j))) {
+        j--;
+        continue;
+      }
+
+      if (s.charAt(i).toLowerCase() !== s.charAt(j).toLowerCase()) {
+        return false;
+      }
+
+      i++;
+      j--;
+    }
+
+    return true;
+  }
+}
+
+function isAlphaNum(char) {
+  let code = char.charCodeAt(0);
+
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // upper alpha (A-Z)
+    !(code > 96 && code < 123)
+  ) {
+    // lower alpha (a-z)
+    return false;
+  }
+
+  return true;
 }
 
 let solution = new Solution();
